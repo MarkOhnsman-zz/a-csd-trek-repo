7f008ec99165136580a77e3048ec242143672eaa
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2021-06-17 10:36:53 -0600

    close sprint 1

--- models/Shield.js
+++ models/Shield.js
@@ -1,10 +1,39 @@
 class Shield {
+  _energyLevel = 5000;
   _isRaised = false;
-  isRaised() {
+  get isRaised() {
     return this._isRaised;
   }
+  get energyLevel() {
+    return this._energyLevel;
+  }
+
   raise() {
     this._isRaised = true;
+  }
+
+  receiveEnergy(energy) {
+    // ANCHOR surplus
+    this._energyLevel += proviedEnergy;
+    if (this._energyLevel > 10000) {
+      returnedEnergy = this._energyLevel - 10000;
+      this._energyLevel = 10000;
+    }
+  }
+
+  provideEnergy(energy) {}
+
+  transferEnergy(proviedEnergy) {
+    let returnedEnergy = 0;
+    this._energyLevel += proviedEnergy;
+    if (this._energyLevel > 10000) {
+      returnedEnergy = this._energyLevel - 10000;
+      this._energyLevel = 10000;
+    } else if (this._energyLevel < 0) {
+      this._energyLevel = 0;
+    }
+
+    return returnedEnergy;
   }
 }
 
--- spec/Trekers/TestSpec.js
+++ spec/Trekers/TestSpec.js
@@ -1,14 +1,51 @@
 let Shield = require("../../models/Shield.js");
 
+let shield;
 describe("shield", () => {
-  it("Starts Down", () => {
-    let shield = new Shield();
-    expect(shield.isRaised()).toBeFalse();
+  // REVIEW test duplications (pre-test)
+  beforeEach(() => {
+    shield = new Shield();
   });
 
-  xit("Can be raised", () => {
-    let shield = new Shield();
+  it("Starts Down", () => {
+    expect(shield.isRaised).toBeFalse();
+  });
+
+  it("Can be raised", () => {
     shield.raise();
-    expect(shield.isRaised()).toBeTrue();
+    expect(shield.isRaised).toBeTrue();
+  });
+
+  it("Initial Energy Level of 5000", () => {
+    expect(shield.energyLevel).toBe(5000);
+  });
+
+  describe("Energy Transfer", () => {
+    it("Can have energy added", () => {
+      shield.transferEnergy(1000);
+      expect(shield.energyLevel).toBe(6000);
+    });
+
+    it("Can have energy removed", () => {
+      shield.transferEnergy(-1000);
+      expect(shield.energyLevel).toBe(4000);
+    });
+
+    it("Cannot have energy increased above 10000", () => {
+      shield.transferEnergy(5001);
+      expect(shield.energyLevel).toBe(10000);
+    });
+
+    it("Cannot have energy decreased below 0", () => {
+      shield.transferEnergy(-5001);
+      expect(shield.energyLevel).toBe(0);
+    });
+
+    it("Returns energy surplus if above 10000", () => {
+      let returned = shield.transferEnergy(6000);
+      expect(returned).toBe(1000);
+    });
+
+    xit("Only returns available energy", () => {});
   });
 });
